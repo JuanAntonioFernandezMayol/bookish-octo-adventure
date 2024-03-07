30f44c61ae2e71a94af31b9d258cf65b560faa97
Juan Antonio Fernandez <121491697+JuanAntonioFernandezMayol@users.noreply.github.com>
2024-03-07 16:54:02 +0100

    Actualizando rama

--- app/src/test/java/entornos/git2/AppTest.java
+++ app/src/test/java/entornos/git2/AppTest.java
@@ -7,8 +7,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    // @test
+    // void gestionarMenu() {
+    // App classUnderTest = new App();
+    // assertEquals(classUnderTest, classUnderTest);
+    // assertEquals(classUnderTest, classUnderTest);
+    // assertEquals(classUnderTest, classUnderTest);
+    // }
+
+    // @test
+    // void gestionarComandas() {
+    // App classUnderTest = new App();
+    // assertEquals(classUnderTest, classUnderTest);
+    // assertEquals(classUnderTest, classUnderTest);
+    // assertEquals(classUnderTest, classUnderTest);
+    // }
 }
