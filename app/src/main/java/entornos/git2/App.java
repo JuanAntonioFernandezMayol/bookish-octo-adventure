a199d2e8edff629b48053315d71be0baa59bad0d
Juan Antonio Fernandez <121491697+JuanAntonioFernandezMayol@users.noreply.github.com>
2024-03-13 16:09:47 +0100

    aa

--- app/src/main/java/entornos/git2/App.java
+++ app/src/main/java/entornos/git2/App.java
@@ -1,19 +1,17 @@
 package entornos.git2;
 
-import java.io.IOException;
 import java.util.HashMap;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         HashMap<String, Menu> menu = new HashMap<>();
-        Menu menu1 = new Menu("Macarrones", 12);
-        Menu menu2 = new Menu("Pizza", 15);
-        Menu menu3 = new Menu("Calzone", 13);
-
+        Menu plato1 = new Menu("Macarrones", 12);
+        Menu plato2 = new Menu("Pizza", 15);
+        Menu plato3 = new Menu("Calzone", 13);
+        menu.put("Macarrones", plato1);
+        menu.put("Pizza", plato2);
+        menu.put("Calzone", plato3);
     }
 
 }
--- app/src/test/java/entornos/git2/AppTest.java
+++ app/src/test/java/entornos/git2/AppTest.java
@@ -7,12 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
-
     // @test
     // void gestionarMenu() {
     // App classUnderTest = new App();
